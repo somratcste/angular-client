aff9d3e8624f9f244214cc8fbdd70b480958bfb7
somratcste <somratcste@gmail.com>
2016-09-22 12:22:11 +0600

    add toast message

--- scripts/controllers/note.js
+++ scripts/controllers/note.js
@@ -3,12 +3,16 @@ angular.module('Client')
 	.controller('IndexNoteCtrl' , function($scope, NoteResource){
 		$scope.Notes = NoteResource.query();
 	})
-	.controller('CreateNoteCtrl', function($scope, NoteResource){
+	.controller('CreateNoteCtrl', function($scope, NoteResource , $location , $timeout){
 		$scope.button = "Save";
 		$scope.title = "Create Note";
 		$scope.Note = {};
 		$scope.saveNote = function(){
 			//console.log($scope.Note);
 			NoteResource.save($scope.Note);
+			Materialize.toast('Note Created' , 5000 , 'green accent-4');
+			$timeout(function(){
+				$location.path('/notes');
+			},1000);
 		};
 	});
