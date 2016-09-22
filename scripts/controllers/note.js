a2a420fffdee42b34f2925017ef82b7c48ae514f
somratcste <somratcste@gmail.com>
2016-09-22 08:34:43 +0600

    add scripts

---
+++ scripts/controllers/note.js
@@ -0,0 +1,4 @@
+'use strict'
+angular.module('Client').controller('IndexNoteCtrl' , function(){
+	
+});
---
+++ scripts/router.js
@@ -0,0 +1,12 @@
+'use strict'
+angular.module('Client',['ngResource','ngRoute'])
+	.config(function($routeProvider){
+		$routeProvider
+		.when('/notes' , {
+			templateUrl : 'views/note/index.html',
+			controller : 'IndexNoteCtrl'
+		})
+		.otherwise({
+			redirectTo : '/'
+		});
+	});
