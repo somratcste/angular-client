6383788caecef821bb801b568a9cfe621276d6f8
somratcste <somratcste@gmail.com>
2016-09-22 08:46:12 +0600

    ng-view

--- scripts/controllers/note.js
+++ scripts/controllers/note.js
@@ -1,4 +1,4 @@
-'use strict'
+
 angular.module('Client').controller('IndexNoteCtrl' , function(){
-	
+
 });
--- scripts/router.js
+++ scripts/router.js
@@ -1,4 +1,4 @@
-'use strict'
+
 angular.module('Client',['ngResource','ngRoute'])
 	.config(function($routeProvider){
 		$routeProvider
