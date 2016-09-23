c4d33bd89ed2472bf84298a017c3117c118d69d8
somratcste <somratcste@gmail.com>
2016-09-23 07:52:08 +0600

    Deleted Note

--- scripts/controllers/note.js
+++ scripts/controllers/note.js
@@ -2,6 +2,16 @@
 angular.module('Client')
 	.controller('IndexNoteCtrl' , function($scope, NoteResource){
 		$scope.Notes = NoteResource.query();
+		$scope.removeNote = function(id) 
+		{
+			NoteResource.delete({
+				id : id
+			});
+			Materialize.toast('Note Deleted' , 5000 , 'green accent-4');
+			$timeout(function(){
+				$location.path('/notes');
+			},1000);
+		};
 	})
 	.controller('CreateNoteCtrl', function($scope, NoteResource , $location , $timeout){
 		$scope.button = "Save";
